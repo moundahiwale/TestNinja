2ebde6f0a05252ab7962cf04a27c693c06406d14
moundahiwale <moundahiwale@yahoo.com>
2020-10-25 16:58:09 +0000

    HousekeeperServiceTests: Add More Interaction tests

--- TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
+++ TestNinja.UnitTests/Mocking/HousekeeperServiceTests.cs
@@ -15,6 +15,7 @@ namespace TestNinja.UnitTests.Mocking
         private readonly Mock<IXtraMessageBox> _messageBox;
         private DateTime _statementDate = new DateTime(2020, 1, 1);
         private readonly Housekeeper _housekeeper;
+        private string _statementFileName;
 
         public HousekeeperServiceTests()
         {
@@ -25,11 +26,16 @@ namespace TestNinja.UnitTests.Mocking
                 _housekeeper
             }.AsQueryable());
 
+            _statementFileName = "fileName";
             _statementGenerator = new Mock<IStatementGenerator>();
+            _statementGenerator
+                .Setup(sg => sg.SaveStatement(_housekeeper.Oid, _housekeeper.FullName, _statementDate))
+                .Returns(() => _statementFileName);
+
             _emailSender = new Mock<IEmailSender>();
             _messageBox = new Mock<IXtraMessageBox>();
 
-            _service = new HousekeeperService(unitOfWork.Object, _statementGenerator.Object, _emailSender.Object, _messageBox.Object);;
+            _service = new HousekeeperService(unitOfWork.Object, _statementGenerator.Object, _emailSender.Object, _messageBox.Object); ;
         }
 
         [Fact]
@@ -51,6 +57,40 @@ namespace TestNinja.UnitTests.Mocking
             _service.SendStatementEmails(_statementDate);
 
             _statementGenerator.Verify(sg => sg.SaveStatement(_housekeeper.Oid, _housekeeper.FullName, _statementDate), Times.Never);
+        }
+
+        [Fact]
+        public void SendStatementEmails_WhenCalled_ShouldEmailStatement()
+        {
+            _service.SendStatementEmails(_statementDate);
+
+            _emailSender.Verify(es => es.EmailFile(_housekeeper.Email, _housekeeper.StatementEmailBody, _statementFileName, It.IsAny<string>()));
+        }
+
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(" ")]
+        [InlineData("")]
+        public void SendStatementEmails_InvalidFileName_ShouldNotEmailStatement(string fileName)
+        {
+            _statementFileName = fileName;
+
+            _service.SendStatementEmails(_statementDate);
+
+            _emailSender.Verify(es => es.EmailFile(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        }
+
+        [Fact]
+        public void SendStatementEmails_EmailSendingFails_DisplayMessageBox()
+        {
+            _emailSender.Setup(es => es.EmailFile(
+                It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()
+                )).Throws<Exception>();
+            
+            _service.SendStatementEmails(_statementDate);
+
+            _messageBox.Verify(mb => mb.Show(It.IsAny<string>(), It.IsAny<string>(), MessageBoxButtons.OK));
         }
     }
 }
