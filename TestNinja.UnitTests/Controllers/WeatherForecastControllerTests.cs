0f0bf2718c77d2e8dac26732f033c4a0a6a1d37a
moundahiwale <moundahiwale@yahoo.com>
2020-10-25 19:22:04 +0000

    Update Weather Forecast Controller Test

--- TestNinja.UnitTests/Controllers/WeatherForecastControllerTests.cs
+++ TestNinja.UnitTests/Controllers/WeatherForecastControllerTests.cs
@@ -1,17 +1,28 @@
+using System;
 using System.Linq;
+using Moq;
 using TestNinja.Controllers;
+using TestNinja.Services;
 using Xunit;
 
 namespace TestNinja.UnitTests.Controllers
 {
     public class WeatherForecastControllerTests
     {
-        private const int _forecastCount = 5;
+        private WeatherForecast[] _weatherForecasts =
+        {
+            new WeatherForecast{Date = DateTime.Now, TemperatureC = -14, Summary = "Mild"},
+            new WeatherForecast{Date = DateTime.Now, TemperatureC = -1, Summary = "Cool"}
+        };
         private readonly WeatherForecastController _controller;
+        private readonly Mock<IWeatherService> _weatherService;
 
         public WeatherForecastControllerTests()
         {
-            _controller = new WeatherForecastController();
+            _weatherService = new Mock<IWeatherService>();
+            _weatherService.Setup(ws => ws.GetForecast()).Returns(_weatherForecasts);
+
+            _controller = new WeatherForecastController(_weatherService.Object);
         }
 
         [Fact]
@@ -19,10 +30,7 @@ namespace TestNinja.UnitTests.Controllers
         {
             var result = _controller.Get();
 
-            // May use any of the following
-            Assert.NotEmpty(result);
-            Assert.Equal(result.Count(), _forecastCount);
-            Assert.IsType<WeatherForecast[]>(result);
+            Assert.Equal(result, _weatherForecasts);
         }
     }
 }
